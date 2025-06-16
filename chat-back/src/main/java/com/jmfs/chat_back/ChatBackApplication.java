09723182b81688d11bd8477542bae0500b93b1bc
João Miguel <joanomiguel@gmail.com>
2025-06-15 21:47:57 -0300

    feat: projeto spring adicionado

---
+++ chat-back/src/main/java/com/jmfs/chat_back/ChatBackApplication.java
@@ -0,0 +1,13 @@
+package com.jmfs.chat_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBackApplication.class, args);
+	}
+
+}
---
+++ chat-back/src/test/java/com/jmfs/chat_back/ChatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmfs.chat_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
