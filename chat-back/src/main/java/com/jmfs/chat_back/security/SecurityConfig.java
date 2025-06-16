eff750f49c75b6694a152e6564fac87b2e92fc04
João Miguel <joanomiguel@gmail.com>
2025-06-16 19:23:09 -0300

    fix: corrigido permissão cors

--- chat-back/src/main/java/com/jmfs/chat_back/security/SecurityConfig.java
+++ chat-back/src/main/java/com/jmfs/chat_back/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jmfs.chat_back.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,20 +30,39 @@ public class SecurityConfig {
 
       @Autowired
       CustomUserDetailsService customUserDetailsService;
+      @Autowired
+      private CorsConfigurationSource corsConfigurationSource;
 
       @Bean
       public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            log.info("[SECURITY] Configuring security filter chain");
-            http
-                        .csrf(csrf -> csrf.disable())
-                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                        .authorizeHttpRequests(authorize -> authorize
-                                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                                    .anyRequest().authenticated())
-                        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-            log.info("[SECURITY] Security filter chain configured successfully");
-            return http.build();
+          log.info("[SECURITY] Configuring security filter chain");
+          http
+              .csrf(csrf -> csrf.disable())
+              .cors(cors -> cors.configurationSource(corsConfigurationSource(http))) // <--- ESSENCIAL
+              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+              .authorizeHttpRequests(authorize -> authorize
+                  .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                  .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                  .anyRequest().authenticated())
+              .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+          log.info("[SECURITY] Security filter chain configured successfully");
+          return http.build();
+      }
+
+      @Bean
+      protected CorsConfigurationSource corsConfigurationSource(HttpSecurity http){
+            log.info("[SECURITY] Configuring HTTP security");
+            CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowedOrigins(List.of("http://localhost:3000"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowCredentials(true); 
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", config);
+
+            return source;
       }
       
       @Bean
--- chat-back/src/main/java/com/jmfs/chat_back/security/SecurityFilter.java
+++ chat-back/src/main/java/com/jmfs/chat_back/security/SecurityFilter.java
@@ -9,10 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Component;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -51,7 +55,8 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) return null;
+        if (authHeader == null)
+            return null;
         return authHeader.replace("Bearer ", "");
     }
 }
